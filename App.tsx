aa7d1c45501544fe6b20bf94e632049195d58d2f
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2021-11-24 09:42:51 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
-import "./App.css";
+
+const Title = styled.h1`
+  color:8257e6;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello World</h1>
+      <h1 className="title">Hello World</Title>
     </div>
   );
 }
