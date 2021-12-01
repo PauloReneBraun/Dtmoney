beb97a28959c427f64f38c4562235c1df62d95c9
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2021-11-30 21:18:06 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 const Title = styled.h1`
   font-size: 64px
   color:8257e6;
-`
+`;
 
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">
+      <Title>
         Hello World
-        <button>Salavr</button>
-        </Title>
+        <button>Salvar</button>
+      </Title>
     </div>
   );
 }
