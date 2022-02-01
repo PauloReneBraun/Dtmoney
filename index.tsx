5aae97d49ea936f6a4a4d3fba8fdac7a29a7cdd2
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2022-02-01 20:07:20 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,14 +1,5 @@
-import logoImg from "../../assets/logo.svg";
-
-import { Container, Content } from "./styles";
-
-export function Header() {
+export function Dashboard() {
   return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
-      </Content>
-    </Container>
+    
   );
 }
